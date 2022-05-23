fba2017c6614f5716546de386c046b51193d3dbe
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-23 12:30:21 -0500

    pf - concatenar
    reutilizando funciones

--- p-estructural.js
+++ p-estructural.js
@@ -2,3 +2,10 @@ let name = "pepito";
 let hello = "hello";
 
 console.log(hello + name);
+
+function saludar (name){
+    return "Hello " + name;
+}
+
+console.log(saludar("juan"));
+console.log(saludar("ramon"));
